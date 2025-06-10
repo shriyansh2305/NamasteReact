627b5bbe2dfe71af0172acbccf890ce0fa4b9035
shriyansh2305 <ssshriyansh23@gmail.com>
2025-06-11 00:11:21 +0530

    igniting app using parcel ep:2

--- E-1/App.js
+++ E-1/App.js
@@ -12,17 +12,20 @@
 
 
 */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const parent = React.createElement("div", {id: "parent"}, [
-    React.createElement("div", {id: "child"}, [
-        React.createElement("h1", {}, "I'm h1 tag"),
-        React.createElement("h2", {}, "I'm h2 tag")
-    ]),
-    React.createElement("div", {id: "child2"}, [
-        React.createElement("h1", {}, "I'm h1 tag"),
-        React.createElement("h2", {}, "I'm h2 tag")
-    ])
-])
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child", key: "child1" }, [
+    React.createElement("h1", { key: "h1-1" }, "I'm h1 tag"),
+    React.createElement("h2", { key: "h2-1" }, "I'm h2 tag")
+  ]),
+  React.createElement("div", { id: "child2", key: "child2" }, [
+    React.createElement("h1", { key: "h1-2" }, "I'm h1 tag"),
+    React.createElement("h2", { key: "h2-2" }, "I'm h2 tag")
+  ])
+]);
+
 // const heading = React.createElement(
 //   "h1",
 //   { id: "heading" },
