dd46d0c52c132a24df7c6df60dc825ae2cc7dc01
shriyansh2305 <ssshriyansh23@gmail.com>
2025-06-14 23:43:28 +0530

    Laying the foundation Lect: 3

--- App.js
+++ App.js
@@ -1,16 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement() => React Element which is an Object => on rendering => HTML Element
+// React Element
+const heading = <h1>Namaste React using JSX 🚀</h1>;
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Namaste React 🚀"
+// React Components
+// Class Based (OLD)
+// React Functional Component (NEW) => A normal JS function, which returns some JSX code/react element
+
+// We can write like this, if we are returning only one liner JSX
+
+const Title = () => <h1>Namaste React 🚀</h1>;
+const HeadingComponent = () => (
+  <div className="container">
+    <Title />
+    <Title></Title>
+    {Title()}
+    {heading}
+    <h1 className="heading">Namaste React From Functional Component</h1>
+  </div>
 );
-// JSX is transpiled before it reaches the JS engine -> done by Parcel using Babel
-
-// JSX => converted to react.createElement by Babel => React Element which is an Object => on rendering => HTML Element
-const jsxHeading = <h1>Namaste React using JSX 🚀</h1>
+// const HeadingComponentSame = () => {
+//   return (
+//     <div>
+//       <h1 className="heading">Namaste React From Same Functional Component</h1>
+//     </div>
+//   );
+// };
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+// root.render(heading);
+root.render(<HeadingComponent />);
+// root.render(<HeadingComponentSame />);
