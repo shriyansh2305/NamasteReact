35346b05631d66fd85e37c57eb3c7c059a37e502
shriyansh2305 <ssshriyansh23@gmail.com>
2025-06-17 01:40:42 +0530

    useState, reconciliation Lect: 5

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,32 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
+import resList from "../utils/mockData"
+import { useState } from "react";
 const Body = () => {
+  // Local State Variable : Super powered variable
+  // const [listOfRestaurants, setListOfRestaurants] = useState(resList)
+  const arr = useState(resList)
+  // const [listOfRestaurants, setListOfRestaurants] = arr;
+  const listOfRestaurants = arr[0];
+  const setListOfRestaurants = arr[1];
+
+  // Normal JS variable
+  // let listOfRestaurants = [];
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(res => res.info.avgRating > 4);
+            // React will re-render the body compoenent
+            setListOfRestaurants(filteredList)
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
